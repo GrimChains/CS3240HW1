3825d8201cae65d1e7efe4312dd930f305d95755
Nick <ngg3vm@virginia.edu>
2013-01-18 10:38:11 -0500

    Added commandline functionality to make testing easier.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <fstream>
 #include "DataPoint.h"
 
 using namespace std;
 
-int main() {
-	DataPoint a;
-	a.setX(3);
-	a.setY(4);
-	DataPoint b;
-	b.setX(0);
-	b.setY(0);
-	cout << b.dist(a) << endl;
+int main(int argc, char * argv[]) {
+    vector<DataPoint> list;
+    int k, M;
+    string name;
+    if (argc == 1) { // if no commandline arguments are provided
+        cout << "How many items do you want displayed?\t";
+        cin >> k;
+        cout << "How many values should be read?\t\t";
+        cin >> M;
+        cout << "Which file?\t\t\t\t";
+        cin >> name;
+    }
+    else if (argc != 4) {
+        cout << "Incorrect number of commandline arguments. Input is \"k, M, file\"" << endl;
+        return 1;
+    }
+    else {
+       k = atoi(argv[1]);
+       M = atoi(argv[2]);
+       name = atoi(argv[3]);
+    }
 	return 0;
 }
