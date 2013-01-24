8636adc10bc14378919fd4986671031a2e3aabba
Nick <ngg3vm@virginia.edu>
2013-01-24 00:47:36 -0500

    Added slightly bugged line parsing and a test file

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main(int argc, char * argv[]) {
-    vector<DataPoint> list;
+    vector< DataPoint* > list;
     int k, M;
     string name;
     if (argc == 1) { // if no commandline arguments are provided
@@ -26,11 +26,43 @@ int main(int argc, char * argv[]) {
        k = atoi(argv[1]);
        M = atoi(argv[2]);
        name = argv[3];
-       cout << "Name: " << name << endl;
     }
     ifstream in;
     in.open(name.c_str());
-    getline(in, name, '.');
+    string temp, input;
+    int count;
+    DataPoint * bus;
+    while (getline(in, input)) {
+        count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input[i] != ' ') {
+                temp += input[i];
+            }
+            else {
+                if (count == 0) {
+                    bus = new DataPoint();
+                    bus->setClass(temp);
+                    count++;
+                }
+                else if (count == 1) {
+                    cout << "temp: " << temp << ". " << i << endl;
+                    bus->setX(atoi(temp.c_str()));
+                    count++;
+                }
+                else if (count == 2) {
+                    bus->setY(atoi(temp.c_str()));
+                    count = 0;
+                }
+                temp = "";
+            }
+        }
+            list.push_back(bus);
+            cout << list.size() << endl;
+            for (int j = 0; j < list.size(); j++) {
+                cout << list.at(j)->getClass() << " " << list.at(j)->getX() << " " << list.at(j)->getY() << endl;
+            }
+        
+    }
     cout << name << endl;
     in.close();
 	return 0;
